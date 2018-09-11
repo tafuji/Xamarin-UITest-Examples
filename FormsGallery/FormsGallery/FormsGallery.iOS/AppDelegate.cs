c0f1b3838b9bcf0d674b3ac40ebe153419d5f340
Takeshi Fujimoto <tafuji@users.noreply.github.com>
2018-09-11 11:26:31 +0900

    Added a compiler detective and Xamarin.UITest.Agent package to the iOS project.

--- FormsGallery/FormsGallery/FormsGallery.iOS/AppDelegate.cs
+++ FormsGallery/FormsGallery/FormsGallery.iOS/AppDelegate.cs
@@ -22,6 +22,11 @@ namespace FormsGallery.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
+            // You need to add this compiler directive by yourself to prevent Xamarin.Calabash assembly from being included in your release build.
+            #if ENABLE_XAMARIN_UITEST
+            Xamarin.Calabash.Start();
+            #endif
+
             global::Xamarin.Forms.Forms.Init();
             Xamarin.FormsMaps.Init();
             LoadApplication(new App());
