18a5c1ec2488bb0ad7453e38a6c7e68b858b9a19
Takeshi Fujimoto <tafuji@users.noreply.github.com>
2018-09-25 15:22:32 +0900

    Addes a sample UI test project

---
+++ HelloXamarin/HelloXamarin/HelloXamarin.UITest/AppInitializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace HelloXamarin.UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.EnableLocalScreenshots().InstalledApp("com.tafuji.HelloXamarin").StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
